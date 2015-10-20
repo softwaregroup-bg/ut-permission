dfbb815f4193750c5e6fccc95dc4fd6f5b6ea0ee
dilyan.vasilev <dilyan.vasilev@gmail.com>
2015-10-20 12:05:45 +0300

    fix(Check for session when the call is from desktop and remove method checkPermission)

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-"use strict";
-var utTemplate = require("ut-template");
-var _ = require("lodash");
-var when = require("when");
+'use strict';
+var utTemplate = require('ut-template');
+var _ = require('lodash');
+var when = require('when');
 
 module.exports = function(templates) {
     templates = _.assign({
-        getUserPermissions :utTemplate.load(require.resolve("./ut/getUserPermissions.sql.marko"))
+        getUserPermissions :utTemplate.load(require.resolve('./ut/getUserPermissions.sql.marko'))
     }, templates || {});
 
     function getParams(params) {
@@ -19,60 +19,57 @@ module.exports = function(templates) {
         return getParams(params);
     }
 
-    function checkPermission(permissions, method){
-        if(Array.isArray(permissions) && permissions.indexOf(method) !== -1){
-            return when.resolve(permissions);
-        } else {
-            return when.reject({
-                code: 403,
-                message: "Access is denied. You do not have permission to perform the requested operation.",
-                errorPrint: "Access is denied. You do not have permission to perform the requested operation.",
-                permissions: permissions
-            });
-        }
-    }
-
-    function loadPermission(session, params){
-        session = session || {};
-        return this.execTemplateRows(templates.getUserPermissions, getParams.call(this, params), true)
-            .then(function(result){
-                session.permissions = [];
-                for(var n= 0, len = result.length; n<len;n++){
-                    if(result[n].code) {
-                        session.permissions.push(result[n].code);
-                    }
-                }
-                return when.resolve(session);
-            });
-    }
-
-    function getPermissions(session) {
+    function getPermissions (session) {
         if(session && session.permissions ){
             return when.resolve(session.permissions);
         } else if(session) {
-            return loadPermission.call(this, session, { userId: session.userId })
-                .then(function(session){
+            return this.execTemplateRows(templates.getUserPermissions, getParams.call(this, { userId: session.userId }), true)
+                .then(function(result){
+                    session.permissions = [];
+                    for(var n= 0, len = result.length; n<len;n++){
+                        if(result[n].code) {
+                            session.permissions.push(result[n].code);
+                        }
+                    }
                     return when.resolve(session.permissions);
                 });
         } else if(!session && this.config && this.config.permission && this.config.permission.requireSession) {
             return when.reject({
                 code: 401,
-                message: "There is no active session.",
-                errorPrint: "There is no active session."
+                message: 'There is no active session.',
+                errorPrint: 'There is no active session.'
             });
         } else {
-            return loadPermission.call(this, session, { name: "anonymous" })
-                .then(function(session){
+            return this.execTemplateRows(templates.getUserPermissions, getParams.call(this, { name: 'anonymous' }), true)
+                .then(function(result){
+                    session.permissions = [];
+                    for(var n= 0, len = result.length; n<len;n++){
+                        if(result[n].code) {
+                            session.permissions.push(result[n].code);
+                        }
+                    }
                     return when.resolve(session.permissions);
                 });
         }
     }
 
     return {
-        check:function(session, method){
+        check: function(session, method){
+            session = session && session.$$ ? session.$$.session : session;
             return getPermissions.call(this, session)
                 .then(function(permissions){
-                    return checkPermission(permissions, method);
+                    if(!method) {
+                        return when.resolve(permissions);
+                    } else if(Array.isArray(permissions) && permissions.indexOf(method) !== -1){
+                        return when.resolve(permissions);
+                    } else {
+                        return when.reject({
+                            code: 403,
+                            message: 'Access is denied. You do not have permission to perform the requested operation.',
+                            errorPrint: 'Access is denied. You do not have permission to perform the requested operation.',
+                            permissions: permissions
+                        });
+                    }
                 });
         },
         getPermissions: getPermissions
