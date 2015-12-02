556e4a3940084570d63288f4a1dcc81b68f3408c
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2015-12-02 11:34:23 +0200

    feat: add linting through npm test and ut-tools/ut-lint

--- index.js
+++ index.js
@@ -5,46 +5,46 @@ var when = require('when');
 
 module.exports = function(templates) {
     templates = _.assign({
-        getUserPermissions :utTemplate.load(require.resolve('./ut/getUserPermissions.sql.marko'))
+        getUserPermissions: utTemplate.load(require.resolve('./ut/getUserPermissions.sql.marko'))
     }, templates || {});
 
     function getParams(params) {
-        var config =  _.assign({ // merge only once
+        var config = _.assign({ // merge only once
 
         }, (this.config && this.config.permission) || {});
-        getParams = function(params) { // lazy initialization
+        var getParamsReturn = function(params) { // lazy initialization
             params.random = Math.random().toString(36).substring(5).toUpperCase();
             return _.defaults(params, config);
-        }
-        return getParams(params);
+        };
+        return getParamsReturn(params);
     }
 
-    function getPermissions (session, $meta) {
-        if(session && session.permissions ){
+    function getPermissions(session, $meta) {
+        if (session && session.permissions) {
             return when.resolve(session.permissions);
-        } else if(session) {
-            return this.execTemplateRows(templates.getUserPermissions, getParams.call(this, { userId: session.userId }), true)
-                .then(function(result){
+        } else if (session) {
+            return this.execTemplateRows(templates.getUserPermissions, getParams.call(this, {userId: session.userId}), true)
+                .then(function(result) {
                     session.permissions = [];
-                    for(var n= 0, len = result.length; n<len;n++){
-                        if(result[n].code) {
+                    for (var n = 0, len = result.length; n < len; n++) {
+                        if (result[n].code) {
                             session.permissions.push(result[n].code);
                         }
                     }
                     return when.resolve(session.permissions);
                 });
-        } else if(!session && this.config && this.config.permission && this.config.permission.requireSession) {
+        } else if (!session && this.config && this.config.permission && this.config.permission.requireSession) {
             return when.reject({
                 code: 401,
                 message: 'There is no active session.',
                 errorPrint: 'There is no active session.'
             });
         } else {
-            return this.execTemplateRows(templates.getUserPermissions, getParams.call(this, { name: 'anonymous' }), true)
-                .then(function(result){
+            return this.execTemplateRows(templates.getUserPermissions, getParams.call(this, {name: 'anonymous'}), true)
+                .then(function(result) {
                     session.permissions = [];
-                    for(var n= 0, len = result.length; n<len;n++){
-                        if(result[n].code) {
+                    for (var n = 0, len = result.length; n < len; n++) {
+                        if (result[n].code) {
                             session.permissions.push(result[n].code);
                         }
                     }
@@ -54,13 +54,13 @@ module.exports = function(templates) {
     }
 
     return {
-        check: function(method, $meta){
+        check: function(method, $meta) {
             var session = $meta.session;
             return getPermissions.call(this, session, $meta)
-                .then(function(permissions){
-                    if(!method) {
+                .then(function(permissions) {
+                    if (!method) {
                         return when.resolve(permissions);
-                    } else if(Array.isArray(permissions) && permissions.indexOf(method) !== -1){
+                    } else if (Array.isArray(permissions) && permissions.indexOf(method) !== -1) {
                         return when.resolve(permissions);
                     } else {
                         return when.reject({
